b7746b3edbb8f6edb9fc36a8ac873e36fe6c51e6
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-02-23 13:57:32 -0800

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,31 +1,36 @@
-letter_list = [ #for letters a - z
-    "♬", "♭", "♮", "♯", "°", "ø",
-    "؂", "≠", "≭", "☧", "☨", "☩",
-    "☫", "☬", "☭", "☯", "☽", "☾",
-    "✙", "✛", "✜", "✝", "✞", "✟",
-    "‡", "♁"
-]
+mapping = {  #for letters a - z
+    "a": "♬", "b": "♭", "c": "♮", "d": "♯", "e": "°", "f": "ø",  
+    "g": "؂", "h": "≠", "i": "≭", "j": "☧", "k": "☨", "l": "☩",  
+    "m": "☫", "n": "☬", "o": "☭", "p": "☯", "q": "☽", "r": "☾",  
+    "s": "✙", "t": "✛", "u": "✜", "v": "✝", "w": "✞", "x": "✟",  
+    "y": "‡", "z": "♁", 
+    #numbers start here
+    "0": "♔", "1": "♕", "2": "♖", "3": "♗", 
+    "4": "♘", "5": "♙", "6": "♚", "7": "♛", "8": "♜", "9": "♞",
+    #special characters start here
+    "!": "♟", "@": "♤", "#": "♠", "$": "♧", "%": "♣",
+     "^": "♡", "&": "♥", "*": "♢","(": "♦", ")": "♩",
+     ".": "♪", "?": "♫"
+}
 
-numbers_list = [ #for numbers 0-9
-    "♔", "♕", "♖", "♗", "♘", 
-    "♙", "♚", "♛", "♜", "♞"
-]
 
-special_characters_list = [ #for special characters !@#$%^&*().?
-     "♟", "♤", "♠", "♧", "♣",
-     "♡", "♥", "♢", "♦", "♩",
-     "♪", "♫"
-]
-
-def apply_substitution():
+def apply_substitution(encrypted_word):
      
+    substituted_word = ''.join(mapping.get(char, char) for char in encrypted_word)
 
+    return substituted_word
+
+def apply_reversed_substitution(encrypted_word):
      
-     print("hello")
+     reversed_mapping = {v : k for k, v in mapping.items()}
+
+     reversed_substituted_word = ''.join(reversed_mapping.get(char, char) for char in encrypted_word)
+
+     return reversed_substituted_word
 
 
 def transpose(text):
-    return text[::-1]
+     return ''.join(reversed(text.strip()))
 
 def shift_and_wrap(text, key):
 
@@ -114,19 +119,36 @@ def shift_and_wrap_decrypted(text, key):
     
 
 def encryption(text, key):
+    print("In Encryption, Text Is: ", text)
+
     transpose_texxt = transpose(text)
+
+    print("After Transposing: ", transpose_texxt)
 
     encrypted_texxt = shift_and_wrap(transpose_texxt, key)
 
-    return encrypted_texxt
+    print("After Shifting and Wrapping: ", encrypted_texxt)
+
+    substituted_encrypted_text = apply_substitution(encrypted_texxt)
+
+    print("After Applying Substitution: ", substituted_encrypted_text)
+
+    return substituted_encrypted_text
 
  
 def decryption(text, key):
-    reverse_transpose_text = transpose(text)
+    print("In Decryption, Text Is: ", text)
 
-    decrypted_texxt = shift_and_wrap_decrypted(reverse_transpose_text, key)
+    reversed_substituted_text = apply_reversed_substitution(text)
+    print("After Reversing Substitution: ", reversed_substituted_text)
 
-    return decrypted_texxt
+    decrypted_text = shift_and_wrap_decrypted(reversed_substituted_text, key)
+    print("After Shifting and Wrapping: ", decrypted_text)
+
+    reverse_transpose_text = transpose(decrypted_text)
+    print("After Transposing: ", reverse_transpose_text)
+
+    return reverse_transpose_text
 
 
     
@@ -154,7 +176,7 @@ def decrypt_file(input_file, output_file, key):
         print(f"Error: {input_file} not found!")
  
 if __name__ == "__main__":
-    print("02112434")
+    print("0211243456")
     print("1. Encrypt a file")
     print("2. Decrypt a file")
  
